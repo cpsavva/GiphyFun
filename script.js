2e143b78ac35e95f60111dd529e5a16f5628d672
cpsavva <csoriasavva@gmail.com>
2016-12-30 23:12:32 -0500

    button tinkering

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
+var newButton;
+var gifData;
+var input;
+var newDiv;
 
+//FUNCTIONS//
 function gifAPI (){
-	var gifData;
-	var input = $("#animal-input").val()
-	var giphyURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=10&api_key=dc6zaTOxFJmzC" 	
-	var newButton = $("<button>").attr("data-name", input).text(input)
-	var newDiv = $("<div>").addClass(" col-md-12 well")
-	var i = 0;
-	
+	input = $(".topic").val();
+	giphyURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=10&api_key=dc6zaTOxFJmzC" 	
+	newButton = $("<button>").addClass("topicBtn topic").text(input).attr("value", input)
+	newDiv = $("<div>").addClass(" col-md-12 well")
+	var i = 0
+
 	$.ajax({url: giphyURL, method: "GET"}).done(function(response){
         console.log(response);
  		console.log(response.data[i].url);
         console.log(response.data[i].rating)
 
-        var gifData = response.data
+        gifData = response.data
 
         for (let i=0; i < gifData.length; i++) {
 
@@ -21,79 +25,45 @@ function gifAPI (){
    		var ratingText = "<p class='rating'>"+ gifData[i].rating+ "</p>"
    		var image = $("<img>").addClass("items").attr("src", gifData[i].images.fixed_width_still.url).data("mode", "0").data("still", gifData[i].images.fixed_width_still.url).data("gif", gifData[i].images.fixed_width.url)
 		
-
-
-
             packageDiv.prepend(image).append(ratingText)
             newDiv.prepend(packageDiv)
 
             $("#images").prepend(newDiv)
-
 		}
- 		$(".buttons").prepend(newButton)
+
+ 		
 	}) /*ajax function*/
-	
-		
 };	
 		
 	
+//MAIN PROCESSES//
 
 $("#add-topic").on("click", function(){
 	event.preventDefault();
 	
 	gifAPI();
-
+	$(".buttons").prepend(newButton)
+	console.log(newButton.text())
+	
 }); /* input click function*/
+
+	
+ $(document).on("click", ".topicBtn", function(){
+ 	console.log($(this).val())
+ 	gifAPI();
+
+ })
+
 $(document).on("click", ".items", function(){
-	// alert("it clicks  " + $(this).data("mode"));
+	console.log("it clicks  " + $(this).data("mode"));
 			if ($(this).data("mode") == 0) {
 				$(this).attr("src", $(this).data("gif")).data("mode", "1")
 			}
 			else {
-				$(this).attr("src", $(this).data("still")).data("view", "0")
+				$(this).attr("src", $(this).data("still")).data("mode", "0")
 			}
 
-
-		}); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}); 
 
 
 });
